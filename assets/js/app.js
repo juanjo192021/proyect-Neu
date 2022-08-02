671a3f7a4284eb46b6689a35c1d0fd99a068bef4
juanjo192021 <94771615+juanjo192021@users.noreply.github.com>
2022-08-02 11:07:28 -0500

    pc a laptop

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,8 @@
+//variables menu desplegable
 let menu = document.getElementById("divContainerMenuBuscarId");
 let altura = menu.offsetTop;
 
-
+//variables imagenes banner
 const seccionImgInicioClass = document.querySelector(".seccionImgInicioClass");
 let divImgBanner = document.querySelectorAll(".divImgBanner");
 let divImgBannerLast = divImgBanner[divImgBanner.length -1 ];
@@ -9,25 +10,29 @@ let divImgBannerLast = divImgBanner[divImgBanner.length -1 ];
 const btnIzq = document.querySelector("#btnIzq");
 const btnDer = document.querySelector("#btnDer");
 
-window.addEventListener("scroll", function(){
-
+//variables iniciar sesion
+// const linkUsuarioId = document.querySelector("#linkUsuarioId");
+const linkUsuarioId = document.querySelector("#linkUsuarioId");
+const divIniRegisClass = document.querySelector(".divIniRegisClass");
+let divIniRegisId = document.getElementById("divIniRegisId");
+const body = document.querySelector("body");
+//Evento menu desplegable 
+window.addEventListener("scroll", () => {
     if(window.pageYOffset > altura){
         menu.classList.add("divContainerMenuBuscar-fixed");
     }else{
         menu.classList.remove("divContainerMenuBuscar-fixed");
     }
-
-
 });
 
+//Evento imagenes banner
 seccionImgInicioClass.insertAdjacentElement("afterbegin",divImgBannerLast);
 
-function moverDerecha(){
+const moverDerecha = () => {
     let divImgBannerFirst = document.querySelectorAll(".divImgBanner")[0];
     seccionImgInicioClass.style.marginLeft = "-200%";
     seccionImgInicioClass.style.transition = "all 1s";
-    setTimeout(() =>{
-        
+    setTimeout(() =>{        
         seccionImgInicioClass.style.transition = "none";
         seccionImgInicioClass.insertAdjacentElement("beforeend", divImgBannerFirst);
         seccionImgInicioClass.style.marginLeft = "-100%";
@@ -39,19 +44,28 @@ const moverIzquierda =() =>{
     seccionImgInicioClass.style.marginLeft = "0";
     seccionImgInicioClass.style.transition = "all 1s";
     setTimeout(() =>{
-        
         seccionImgInicioClass.style.transition = "none";
         seccionImgInicioClass.insertAdjacentElement("afterbegin",divImgBannerLast);
         seccionImgInicioClass.style.marginLeft = "-100%";
     }, 1000);
 }
-let auto = setInterval(() => {
-    moverDerecha();
-}, 7000);
-
-btnDer.addEventListener("click",() =>{      
+btnDer.addEventListener("click",() =>{ 
     moverDerecha();
 });
 btnIzq.addEventListener("click",() =>{
     moverIzquierda();
 });
+
+
+linkUsuarioId.addEventListener("click", () =>{
+    divIniRegisId.classList.add("divIniRegisClass-fixed");
+    setTimeout(() => {
+        divIniRegisId.classList.remove("divIniRegisClass-fixed");
+    }, 5000);
+});
+
+
+// linkUsuarioId.addEventListener("dblclick", () =>{
+//     divIniRegisId.classList.remove("divIniRegisClass-fixed");
+// })
+
